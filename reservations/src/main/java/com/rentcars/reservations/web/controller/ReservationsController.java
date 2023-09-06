4c8cd6e81597f6a82edf91e422674bc49aabc87d
Anne Chollet <anne.chollet@le-campus-numerique.fr>
2023-09-06 12:09:02 +0200

    CRUD parametre & testé dans postman = OK

--- reservations/src/main/java/com/rentcars/reservations/web/controller/ReservationsController.java
+++ reservations/src/main/java/com/rentcars/reservations/web/controller/ReservationsController.java
@@ -1,11 +1,13 @@
 package com.rentcars.reservations.web.controller;
 
+import com.rentcars.reservations.web.model.Reservations;
 import com.rentcars.reservations.web.repository.ReservationsRepository;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
 
 
 @RestController
@@ -18,8 +20,41 @@ public class ReservationsController {
 
     public ReservationsController(ReservationsRepository reservationsRepository) {this.reservationsRepository = reservationsRepository;}
 
+    @ApiOperation(value = "Renvoyer la liste des réservations")
+    @GetMapping
+    public List<Reservations> listReservations() {
+        return reservationsRepository.findAll();
+    }
+
+    @ApiOperation(value = "Renvoyer les infos d'une réservation correspondant à l'identifiant (id) choisi")
+    @GetMapping("/{id}")
+    public Reservations getReservations(@PathVariable int id) {
+        return reservationsRepository.findById(id);
+    }
 
 
+    @ApiOperation(value = "Ajoute une nouvelle réservation")
+    @PostMapping
+    public Reservations addReservations(@RequestBody Reservations newReservations) {
+       // exceptionValidation(newReservation);
+        //Si true == save la nouvelle reservation :
+        return reservationsRepository.save(newReservations);
+    }
+
+
+
+    @ApiOperation(value = "Modifier une réservation de la liste")
+    @PutMapping("/{id}")
+    public Reservations updateReservation (@PathVariable int id, @RequestBody Reservations reservations) {
+        reservations.setId(id);
+        return reservationsRepository.save(reservations);
+    }
+
+    @ApiOperation(value = "Supprimer une réservation de la liste")
+    @DeleteMapping("/{id}")
+    public void deleteReservations(@PathVariable int id) {
+        reservationsRepository.deleteById(id);
+    }
 
 
 
---
+++ reservations/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO reservations (start_date, end_date, client_id, registration_vehicle)
+VALUES
+    ('2023-08-30T09:24:37.816+00:00',
+     '2023-09-30T09:24:37.816+00:00',
+    1,
+     'BC-123-XD'
+    );
